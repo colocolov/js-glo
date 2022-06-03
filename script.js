6781c7b0148d69b15da5d82a118769be5ddad61d
Stas Colocolov <colocolov.stas@gmail.com>
2022-06-03 23:58:57 +0300

    Homework lesson 6

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-let userNumber;
+"use strict";
 
-const isNumber = function (num) {
-  return !isNaN(parseFloat(num)) && isFinite(num);
+const random = function () {
+  let begin = confirm("Угадаешь число?");
+
+  function getNumber() {
+    let num = Math.floor(Math.random() * 100);
+    let answer;
+    console.log(num);
+
+    const isNumber = function (num) {
+      return !isNaN(parseFloat(num)) && isFinite(num);
+    };
+
+    function confirm() {
+      if (answer > num) {
+        alert("Загаданное число меньше");
+        number();
+      } else if (answer < num) {
+        alert("Загаданное число больше");
+        number();
+      } else if (isNumber(answer) === false) {
+        alert("Введи число!");
+        number();
+      }
+    }
+
+    function number() {
+      answer = prompt("Угадай число от 1 до 100");
+      if (answer == null) {
+        alert("Игра окончена");
+      } else if (Number(answer) === num) {
+        alert("Поздравляю, Вы угадали!!!");
+      } else if (answer !== num) {
+        confirm();
+      }
+    }
+    number();
+  }
+
+  if (begin == true) {
+    getNumber();
+  } else {
+    alert("Игра окончена!");
+  }
 };
 
-const getNumber = function () {
-  do {
-    userNumber = prompt("Введите число");
-    if (userNumber === null) {
-      continue;
-    }
-    userNumber.trim();
-  } while (!isNumber(userNumber));
-
-  return userNumber.trim();
-};
-
-console.log(getNumber());
-
-/*
- *
- */
-let arr = [22, 44, 45, 52, 36, 586, 241];
-
-arr.forEach((item) => {
-  item = item + "";
-  if (item.charAt(0) == 2 || item.charAt(0) == 4) {
-    console.log(item);
-  }
-});
-
-/**
- *
- */
-for (let i = 2; i < 101; i++) {
-  let num = 1;
-  for (let j = 2; j <= i / 2 && num == 1; j++) {
-    if (i % j == 0) {
-      num = 0;
-    }
-  }
-  if (num == 1) {
-    console.log(i);
-  }
-}
+random();
