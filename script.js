f62c36ea573d22159ae77e4f3cd0bd853c03ea48
Stas Colocolov <colocolov.stas@gmail.com>
2022-06-08 23:48:48 +0300

    DOne lesson 9 Hard

--- script.js
+++ script.js
@@ -1,50 +1,85 @@
 "use strict";
 
-const data = document.getElementById("week");
-const todayIs = new Date();
-let days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-let months = [
-  "Января",
-  "Февраля",
-  "Марта",
-  "Апреля",
-  "Мая",
-  "Июня",
-  "Июля",
-  "Августа",
-  "Сентября",
-  "Октября",
-  "Ноября",
-  "Декабря",
-];
-// new Date(year, month[, day[, hour[, minute[, second[, millisecond]]]]]);
+const getDate = function () {
+  const data = document.getElementById("week");
+  const todayIs = new Date();
+  let days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+  let months = [
+    "Января",
+    "Февраля",
+    "Марта",
+    "Апреля",
+    "Мая",
+    "Июня",
+    "Июля",
+    "Августа",
+    "Сентября",
+    "Октября",
+    "Ноября",
+    "Декабря",
+  ];
 
-const dayOfWeek = {
-  isWeekend: function () {
+  function dateA() {
     let newdiv = document.createElement("div");
-    // week.forEach(function (item, i) {
 
-    // if (i == todayIs.getDay() - 1) {
-    //   newdiv.classList.add("strong");
-    //   newdiv.textContent = week[i];
-    // }
-    // if (item == "Saturday" || item == "Sunday") {
-    //   newdiv.classList.add("italic");
     newdiv.textContent =
       "Сегодня " +
       days[todayIs.getDay()] +
       ", " +
-      todayIs.getMonth() +
+      todayIs.getDate() +
       " " +
       months[todayIs.getMonth()] +
       " " +
       todayIs.getFullYear() +
-      " года ";
-    // } else {
-    //   newdiv.textContent = week[i];
-    // }
-    // });
+      " года, " +
+      todayIs.getHours() +
+      hours() +
+      todayIs.getMinutes() +
+      " минут " +
+      todayIs.getSeconds() +
+      " секунд";
+
     data.appendChild(newdiv);
-  },
+  }
+
+  function hours() {
+    if (todayIs.getHours() == 1 || todayIs.getHours() == 21) {
+      return " час ";
+    } else if (todayIs.getHours() >= 2 && todayIs.getHours() <= 4) {
+      return " часа ";
+    } else if (todayIs.getHours() >= 22 && todayIs.getHours() <= 24) {
+      return " часа ";
+    } else {
+      return " часов ";
+    }
+  }
+
+  function dateB() {
+    let newdiv = document.createElement("div");
+
+    newdiv.textContent =
+      zero(todayIs.getDay()) +
+      "." +
+      zero(todayIs.getMonth()) +
+      "." +
+      todayIs.getFullYear() +
+      " - " +
+      zero(todayIs.getHours()) +
+      ":" +
+      zero(todayIs.getMinutes()) +
+      ":" +
+      zero(todayIs.getSeconds());
+
+    data.appendChild(newdiv);
+  }
+
+  function zero(number) {
+    return (number = number < 10 ? "0" + number : number);
+  }
+
+  dateA();
+  dateB();
 };
-dayOfWeek.isWeekend();
+
+setInterval(getDate, 1000);
+// getDate();
